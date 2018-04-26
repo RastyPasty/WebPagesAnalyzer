d4fefbaffc0e4c2048352084a989e2d2d89719aa
Serge <rastypasty@gmail.com>
2018-04-26 18:17:31 +0300

    Add files via upload

---
+++ WebPagesAnalyzer/Consts.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPagesAnalyzer
+{
+    public static class Consts
+    {
+        public static int SaltLenght = 10;
+        public static string SecretKey = "v8Qv19DMnB";
+    }
+}
---
+++ WebPagesAnalyzer/Startup.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ORM;
+using Microsoft.EntityFrameworkCore;
+using WebPagesAnalyzer.Services.Interfaces;
+using WebPagesAnalyzer.Services;
+using WebPagesAnalyzer.Repositories.Interfaces;
+using Newtonsoft.Json;
+
+namespace WebPagesAnalyzer
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<WebPagesAnalyzerContext>(options => options.UseSqlServer("Server=tcp:rastypasty.database.windows.net,1433;Initial Catalog=WebPagesAnalyzer;Persist Security Info=False;User ID=rastypasty;Password=QAWSedrftg77;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;"));
+
+            services.AddTransient<IPageHandlerService, PageHandlerService>();
+            services.AddTransient<ICryptoService, CryptoService>();
+            services.AddTransient<IFetcherService, FetcherService>();
+            services.AddTransient<IParserService, ParserService>();
+            services.AddTransient<IWordRepository, WordRepository>();
+            
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    options.SerializerSettings.Formatting = Formatting.Indented;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
