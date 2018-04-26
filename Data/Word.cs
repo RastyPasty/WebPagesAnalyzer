1cebed92887e7aad746640d274089ea182209d7e
Serge <rastypasty@gmail.com>
2018-04-26 18:11:33 +0300

    Add files via upload

---
+++ Data/Word.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data
+{
+    public class Word
+    {
+        [StringLength(100)]
+        public string Id { get; set; }
+
+        [StringLength(250)]
+        public string Data { get; set; }
+
+        public int Count { get; set; }
+    }
+}
